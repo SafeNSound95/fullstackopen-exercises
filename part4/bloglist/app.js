d7c7bd109456b17a414170b69ff42f82ff3fc440
Saif Shalan <safensound95@gmail.com>
2025-07-19 16:20:29 +0300

    refactor: modularized blog backend, initial GET endpoint complete

---
+++ part4/bloglist/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+
+const blogsRouter = require('./controllers/blogs')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+const connect = async () => {
+  try {
+    await mongoose.connect(config.MONGODB_URI)
+    logger.info('connected to MongoDB ')
+  } catch(error) {
+    logger.error('error connection to MongoDB:', error.message)
+  }
+}
+
+connect()
+
+const app = express()
+
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
